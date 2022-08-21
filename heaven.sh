fd31cca95d57c9adf24a5af6a90a186109348dc8
Jon <jofasakin@gmail.com>
2022-08-21 21:49:18 +0000

    How to make heaven

---
+++ heaven.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# How to knw that you will  make heaven after death
+echo "What is your name"
+read name
+echo "what is your date of birth"
+read date
+echo "what is the name of your saviour"
+read name
+echo "What date did you give your life to your saviour"
+read date
+echo "As much you can say, how do you rate your love for God"
+read rate
+echo "do you have secret sins you still commit"
+echo "if your answer is yes, then you need to confess the sins"
+echo "Congratulations, the gate of heaven will be opened for you"
+
+
+
